a031a11596b0ba8189f21d01777db4c18645ec13
David van der Spoel <spoel@xray.bmc.uu.se>
2016-03-01 11:47:13 +0100

    Moved storing total charge in gaussformat.cpp
    The total charge in an OBMol may be changed by EndModify or
    other routines involved with changing bondorder and or atom types.
    This patch moves the assignment to the end of the file to make
    sure the total charge does not get lost.
    Moved multiplicity storage down as well.

--- src/formats/gaussformat.cpp
+++ src/formats/gaussformat.cpp
@@ -441,8 +441,8 @@ namespace OpenBabel
     double x,y,z;
     OBAtom *atom;
     vector<string> vs,vs2;
-    int charge = 0;
-    unsigned int spin = 1;
+    int total_charge = 0;
+    unsigned int spin_multiplicity = 1;
     bool hasPartialCharges = false;
     string chargeModel; // descriptor for charges (e.g. "Mulliken")
 
@@ -577,9 +577,8 @@ namespace OpenBabel
             tokenize(vs, buffer, " \t\n");
             if (vs.size() == 6)
               {
-                charge = atoi(vs[2].c_str());
-                spin = atoi(vs[5].c_str());
-                mol.SetTotalSpinMultiplicity(spin);
+                total_charge = atoi(vs[2].c_str());
+                spin_multiplicity = atoi(vs[5].c_str());
               }
 
             ifs.getline(buffer,BUFF_SIZE);
@@ -1257,7 +1256,12 @@ namespace OpenBabel
       dp->SetOrigin(fileformatInput);
       mol.SetData(dp);
     }
+<<<<<<< HEAD
     mol.SetTotalCharge(charge);
+=======
+    mol.SetTotalCharge(total_charge);
+    mol.SetTotalSpinMultiplicity(spin_multiplicity);
+>>>>>>> Moved storing total charge in gaussformat.cpp
 
     mol.SetTitle(title);
     return(true);
